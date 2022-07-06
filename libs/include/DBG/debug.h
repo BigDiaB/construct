c470828c6a4ceb358e74beb79b66ea71d6c785af
Benjamin Peter Emde <72041718+BigDiaB@users.noreply.github.com>
2022-07-06 21:48:21 +0200

    update in debug.h

--- libs/include/DBG/debug.h
+++ libs/include/DBG/debug.h
@@ -11,11 +11,11 @@ extern "C" {
 #endif
 
 void* DEBUG_MEMmalloc(size_t size, char* file, int line);
-void DEBUG_MEMfree(void* ptr);
+void DEBUG_MEMfree(void* ptr, char* file, int line);
 void* DEBUG_MEMrealloc(void* old_ptr,size_t new_size, char* file, int line);
 void DEBUG_MEMeval();
 #define malloc(X)	 DEBUG_MEMmalloc(X,__FILE__,__LINE__)
-#define free(X)		 DEBUG_MEMfree(X)
+#define free(X)		 DEBUG_MEMfree(X,__FILE__,__LINE__)
 #define realloc(X,Y) DEBUG_MEMrealloc(X,Y,__FILE__,__LINE__)
 
 
